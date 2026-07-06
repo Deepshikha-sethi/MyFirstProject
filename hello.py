c2cfdad56b1db991be1aa1e4de26045f4afbf643
Deepshikha Sethi <deepshikha.sethi123@gmail.com>
2026-07-06 11:29:12 +0530

    This is version 2

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 print("Welcome to GitHub")
+print("This is version 2")
+print("Git is a Version Control System — software that tracks\
+      every change you make to your code, so nothing is ever truly lost.")
+print("v3- This is version 3")
